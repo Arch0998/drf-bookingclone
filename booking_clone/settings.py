99665d338b9f956ce4c4ef17235c46198aa6b6e1
Arthur Oleinikov <arch0998@gmail.com>
2025-09-17 02:45:53 +0300

    Fix typo in app name

--- booking_clone/settings.py
+++ booking_clone/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "hotels",
-    "booking",
+    "bookings",
     "reviews",
 ]
 
@@ -119,3 +119,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
