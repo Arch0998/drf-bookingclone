24ad887b6e2edcc4d1b83b7ad9dc20ee1eef717d
Arthur Oleinikov <arch0998@gmail.com>
2025-09-26 19:28:33 +0300

    Add Docker support with PostgreSQL configuration and environment settings

--- booking_clone/asgi.py
+++ booking_clone/asgi.py
@@ -1,16 +1,8 @@
-"""
-ASGI config for booking_clone project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking_clone.settings")
+settings_module = os.environ.get("DJANGO_SETTINGS_MODULE", "booking_clone.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking_clone.settings")
+    settings_module = os.environ.get(
+        "DJANGO_SETTINGS_MODULE", "booking_clone.settings.dev"
+    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
